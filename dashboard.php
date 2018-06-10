516982458d0e15446f92a2fe6752ee3c01aa8373
Christopher <christopherballenger@gmail.com>
2018-06-09 22:16:57 -0400

    The header now displays everyone on that chapter, and the sidebar displays the logged-in user's information.

--- dashboard.php
+++ dashboard.php
@@ -25,23 +25,14 @@
           $sql = <<<SQL
             SELECT * FROM profiles WHERE bdpa_chapter_key = 2;
 SQL;
-          $chaptersql = <<<SQL
+          $chaptersql = <<<CHSQL
             SELECT * FROM bdpa_chapters WHERE bdpa_chapter_key = 2;
-SQL;
+CHSQL;
         $result = $conn->query($sql);
         $chapterresult = $conn->query($chaptersql);
 
-        $x = 0;
-
-        $row = $result->fetch_assoc();
-        echo $row['first_name'];
-
-
-        while ($x < $result->num_rows) {
-          echo <<<PERSON
-
-PERSON;
-          $x++;
+        while ($row = $result->fetch_assoc()) {
+          echo "Name: " . $row['first_name'] . " " . $row['last_name'] . "<hr>";
         }
         ?>
       </div>
@@ -49,7 +40,21 @@ PERSON;
     <div class="card col-md-4">
       <div class="card-header">My Profile <a href="forms/profile_form.php" style="float: right">Edit</a></div>
       <div class="card-body">
+      <?php
+        $username = $_SESSION['username'];
+        $profilesql = <<<SQL
+          SELECT * FROM profiles WHERE user_id="$username";
+SQL;
+        $profileresult = $conn->query($profilesql);
 
+        while ($row = $profileresult->fetch_assoc()) {
+          echo "Username: " . $row['user_id'] .
+              "<hr> Email: " . $row['email'] .
+              "<hr> Phone: " . $row['phone'] .
+              "<hr> Current Job: " . $row['current_job'] .
+              "<hr> Years Participated" . $row['years_participated'];
+        }
+      ?>
       </div>
     </div>
 
--- db.php
+++ db.php
@@ -1,9 +1,7 @@
 <?php $conn = new mysqli('localhost', 'root', '', 'bdpa');
 
 if ($conn->error) {
-  echo "This didn't work.";
-} else {
-  echo "It worked.";
+  echo "The database connection failed.";
 }
 
 ?>
